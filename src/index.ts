89a06847899e37d8c9fcb0deaf15199571357a67
rprominski <rprominski@man.poznan.pl>
2020-10-07 15:56:01 +0200

    preparing files for npm publish

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { PieceColor } from './Piece/PieceColor';
+import { PieceType } from './Piece/PieceType';
+import Piece from './Piece/Piece';
+import FENParser from './FENParser/FenParser';
+import { FENParserError, GamePosition } from './FENParser/FenParser';
+import PositionGenerator from './PositionGenerator/PositionGenerator';
+
+export {
+    PieceColor,
+    PieceType,
+    Piece,
+    FENParser,
+    FENParserError,
+    GamePosition,
+    PositionGenerator
+}
